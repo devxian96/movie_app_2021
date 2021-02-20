1613846f00fc165b57cde0fc61fd50aa7db5bdb9
DevXian <coolman555@me.com>
2021-02-21 00:57:20 +0900

    05-2

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ class App extends React.Component {
   };
 
   add = () => {
-    console.log("add");
+    this.setState((current) => ({ count: current.count + 1 }));
   };
 
   minus = () => {
-    console.log("minus");
+    this.setState((current) => ({ count: current.count - 1 }));
   };
 
   render() {
