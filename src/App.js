9d737e94c121f17c653299bcfc191d373e08c3f4
DevXian <coolman555@me.com>
2021-02-21 15:32:31 +0900

    06-2

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
 import React from "react";
+import axios from "axios";
+
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
-
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
 
-  add = () => {
-    this.setState((current) => ({ count: current.count + 1 }));
-  };
-
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    this.setState({ movies, isLoading: false });
+    /*
+      .then((res) => {
+        this.setState({ isLoading: false, movies: res.data });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+      */
   };
 
   componentDidMount() {
-    console.log("component rendered");
-  }
-
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-
-  componentWillUnmount() {
-    console.log("Goodbye, cruel world");
+    // 영화 데이터 로딩!
+    this.getMovies();
   }
 
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
